379481b34e4606151204cbf9622356e54a248918
Hackbright Student <no-reply@hackbrightacademy.com>
2019-02-06 17:56:38 -0800

    Updated registration

--- model.py
+++ model.py
@@ -58,9 +58,20 @@ class Rating(db.Model):
     rating_id = db.Column(db.Integer,
                           autoincrement=True,
                           primary_key=True)
-    movie_id = db.Column(db.Integer)
-    user_id = db.Column(db.Integer)
+    movie_id = db.Column(db.Integer,
+                         db.ForeignKey('movies.movie_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     score = db.Column(db.Integer)
+
+     # Define relationship to user
+    user = db.relationship("User",
+                           backref=db.backref("ratings",
+                                              order_by=rating_id))
+
+    # Define relationship to movie
+    movie = db.relationship("Movie",
+                            backref=db.backref("ratings",
+                                               order_by=rating_id))
 
     def __repr__(self):
         """Provide helpful representation when printed."""
